d5c9c5836afcd9d3c708a7d4defbdf086fd63fc8
Carlos Henrique Gava <carlos.gava@gmail.com>
2024-04-16 17:49:08 -0300

    Initial Commit

---
+++ Face RecognitionUITests/Face_RecognitionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Face_RecognitionUITestsLaunchTests.swift
+//  Face RecognitionUITests
+//
+//  Created by Carlos Henrique Gava on 14/04/24.
+//
+
+import XCTest
+
+final class Face_RecognitionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
